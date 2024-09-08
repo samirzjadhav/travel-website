ff7e659af59c2f4827a3006354534fb68dad98c4
samir jadhav <samirzjadhav@gmail.com>
2024-09-08 21:45:56 +0530

    feat: added new component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Component } from "react";
 import "./NavbarStyle.css";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <>
+      <nav className=""></nav>
+    </>
+  );
 };
 
 export default Navbar;
