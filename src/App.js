4f4e759dcc7d00e72de18187e26a43ca777800e4
samir jadhav <samirzjadhav@gmail.com>
2024-09-09 22:52:18 +0530

    feat: added new thing it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Component } from "react";
 import "./NavbarStyle.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-      <nav className=""></nav>
+      <nav className="navbarItem">
+        <div className="navbar-logo">Trippy</div>
+        <ul className="nav-menu">
+          <li>
+            <Link>
+              <a href="">
+                <i>Home</i>
+              </a>{" "}
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
