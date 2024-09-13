3cec9f4c4660ca45e78fc6d2882a07e32e56b94d
samir jadhav <samirzjadhav@gmail.com>
2024-09-13 20:19:19 +0200

    feat: added new navbar on the sction

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Component } from "react";
+import { MenuItems } from "./MenuItems";
 import "./NavbarStyle.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="navbarItem">
+      <nav className="navbarItems flex justify-between items-center px-[30px] shadow-xl w-[95%] h-[80px] rounded-xl">
         <div className="navbar-logo">Trippy</div>
         <ul className="nav-menu">
-          <li>
-            {/* <Link to="/home"> */}
-            <a href="">
-              <i>Home</i>
-            </a>{" "}
-            {/* </Link> */}
-          </li>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a href="/" className=" flex gap-[10px] items-center">
+                  <i>{item.icon}</i>
+                  <i>{item.title}</i>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </>
