97ff86e0a4fc80b50d113c194be25dc60ed3a032
samir jadhav <samirzjadhav@gmail.com>
2024-09-14 20:19:19 +0200

    feat:added new navbar style on it

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,12 +8,18 @@ const Navbar = () => {
   return (
     <>
       <nav className="navbarItems flex justify-between items-center px-[30px] shadow-xl w-[95%] h-[80px] rounded-xl">
-        <div className="navbar-logo">Trippy</div>
-        <ul className="nav-menu">
+        <div className="navbar-logo text-[#222] font-bold text-[32px] cursor-pointer">
+          Trippy
+        </div>
+        <ul className="nav-menu grid grid-cols-6 gap-[10px] items-center">
           {MenuItems.map((item, index) => {
             return (
               <li key={index}>
-                <a href="/" className=" flex gap-[10px] items-center">
+                <a
+                  href="/"
+                  className="flex gap-[10px] items-center text-[18px] pr-[10px] hover:bg-[#01959a] hover:text-white hover:rounded-sm hover:transition-all hover:px-[10px] hover:py-[10px] hover:ease-in-out hover:delay-75"
+                  id={item.cName}
+                >
                   <i>{item.icon}</i>
                   <i>{item.title}</i>
                 </a>
