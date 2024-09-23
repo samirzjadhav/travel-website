c160230e5da585d91f9f17fa28511fbbfb685e34
samir jadhav <samirzjadhav@gmail.com>
2024-09-23 13:19:19 +0200

    feat: added new dynamic section for changes

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,14 +1,14 @@
 import React from "react";
 import HeroImg from "../assets/1.jpg";
 
-const Hero = () => {
+const Hero = (props) => {
   return (
     <>
-      <div className="hero">
-        <img src={HeroImg} alt="hero" className="w-full h-auto" />
+      <div className={props.cName}>
+        <img src={props.heroImg} alt="hero" className="w-full h-auto" />
         <div className="hero-text text-center">
-          <h1 className="text-4xl font-bold">Your Journey Your Story</h1>
-          <p className="text-lg mt-4">Choose Your Favourite Destination</p>
+          <h1 className="text-4xl font-bold">{props.title}</h1>
+          <p className="text-lg mt-4">{props.discri}</p>
           <a
             href="#"
             className="inline-block mt-6 px-6 py-3 bg-blue-500 text-white rounded-full"
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Hero from "../Components/Hero";
+import HeroImg from "../assets/1.jpg";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Hero
+        cName="hero"
+        heroImg={HeroImg}
+        title="Your Journey Your Story"
+        discri="Choose Your Favourite Destination."
+      />
     </>
   );
 };
