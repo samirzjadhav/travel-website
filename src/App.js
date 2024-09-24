9cdd747617cc9c2f27e5255a498a7e46bfeff841
samir jadhav <samirzjadhav@gmail.com>
2024-09-24 19:31:25 +0530

    feat: added new routes for section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import { Route, Routes } from "react-router";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Service from "./routes/Service";
+import Contact from "./routes/Contact";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contack" element={<Contact />} />
+      </Routes>
       <Navbar />
     </div>
   );
