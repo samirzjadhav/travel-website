1f3c1c5a07b9d71b4e672fa6fa3409392cb30646
samir jadhav <samirzjadhav@gmail.com>
2024-09-27 19:34:14 +0530

    feat: added new thing in about section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,7 +5,7 @@ const Hero = (props) => {
   return (
     <>
       <div className={props.cName}>
-        <img src={props.heroImg} alt="hero" className="h-[100vh]" />
+        <img src={props.heroImg} alt="hero" className={props.class} />
         <div className="hero-text text-center">
           <h1 className="text-4xl font-bold">{props.title}</h1>
           <p className="text-lg mt-4">{props.discri}</p>
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Hero from "../Components/Hero";
+import AboutImg from "../assets/night.jpg";
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <Hero cName="hero-mid" heroImg={AboutImg} title="About" url="/about" />
+    </>
+  );
+};
 
-export default About
+export default About;
